3968f44d7be18b16541a83434f1d5c59c42c2d94
Luydmila Kolesnikova <Lujdmila-Kolomna@yandex.ru>
2023-11-03 20:37:25 +0300

    add search at github.io

--- src/api/usersAPI.tsx
+++ src/api/usersAPI.tsx
@@ -1,12 +1,18 @@
 import { instance } from "./instanceAPI";
 import { UsersList } from "../redux/users-reducer";
+import { initialState } from "../redux/initialState";
 
 interface usersResponse {
   data: UsersList;
 }
 
 export const usersAPI = {
-  getUsers(name: string): Promise<usersResponse> {
-    return instance.get(`?term=${name}`);
+  getUsers(name: string): Promise<usersResponse> | usersResponse {
+    if (document.location.host.endsWith('github.io')) {
+      return {data: initialState.filter(user => user.name.toLowerCase().includes((name).toLowerCase()))};
+    } 
+    return instance.get(`?term=${name}`)
   },
 };
+
+
